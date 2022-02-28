b876355d954a835023d52c709d8b54af712c71cd
Casper Nybroe <casper.nybroe@cb.dk>
2022-02-28 22:31:27 +0100

    Added stats

--- plant.py
+++ plant.py
@@ -9,6 +9,11 @@ wallet_public_addr = "0x361472B5784e83fBF779b015f75ea0722741f304"
 min_plant_amount = 3.00
 loop_sleep_seconds = 5 # 60*60 # One hour
 margin_of_error = 0.01
+seeds_per_day_per_plant = 86400
+seeds_per_hour_per_plant = seeds_per_day_per_plant / 24
+seeds_per_second_per_plant = seeds_per_hour_per_plant / 60 / 60
+
+seeds_per_plant = 2600000
 
 # load private key
 wallet_private_key = open('key.txt', "r").readline()
@@ -27,6 +32,9 @@ def seeds_for_1_plant():
 def available_seeds():
     return garden_contract.functions.getUserSeeds(wallet_public_addr).call()
 
+def planted_plants():
+    return garden_contract.functions.hatcheryPlants(wallet_public_addr).call()
+
 def plant():
     txn = garden_contract.functions.plantSeeds(wallet_public_addr).buildTransaction(c.get_tx_options(wallet_public_addr, 500000))
     return c.send_txn(txn, wallet_private_key)
@@ -44,23 +52,31 @@ def countdown(t):
 while True:
     seedsFor1Plant = seeds_for_1_plant()
     available = available_seeds()
-    available_plants = available / seedsFor1Plant
+    plantedPlants = planted_plants()
+    availablePlants = available / seedsFor1Plant
 
+    plantsNeededForPlanting = (min_plant_amount + margin_of_error) - availablePlants
+    seedsNeededForPlanting = plantsNeededForPlanting * seeds_per_plant
+    seedsPerSecond = seeds_per_second_per_plant * plantedPlants
+    secondsUntilNextPlanting = seedsNeededForPlanting / seedsPerSecond
 
     dateTimeObj = datetime.now()
     timestampStr = dateTimeObj.strftime("[%d-%b-%Y (%H:%M:%S)]")
     
     print("********** STATS *******")
     print(f"{timestampStr} Seeds for 1 plant: {seedsFor1Plant:.2f}")
+    print(f"{timestampStr} Seeds per second: {seedsPerSecond:.2f}")
+    print(f"{timestampStr} Planted plants: {plantedPlants:.2f}")
     print(f"{timestampStr} Available seeds: {available:.2f}")
-    print(f"{timestampStr} Available plants: {available_plants:.2f}")
+    print(f"{timestampStr} Available plants: {availablePlants:.2f}")
     print(f"{timestampStr} Margin of error: {margin_of_error:.2f}")
-    print(f"{timestampStr} Plants needed before planting: {(min_plant_amount + margin_of_error) - available_plants:.2f}")
+    print(f"{timestampStr} Plants needed before planting: {plantsNeededForPlanting:.2f}")
+    print(f"{timestampStr} Seconds until next planting: {secondsUntilNextPlanting:.2f}")
     print("************************")
     
-    if available_plants >= min_plant_amount and available_plants < (min_plant_amount + margin_of_error):
+    if availablePlants >= min_plant_amount and availablePlants < (min_plant_amount + margin_of_error):
         # plant()
-        print(f"{timestampStr} Planted! {available_plants:.2f} added to garden.")
+        print(f"{timestampStr} Planted! {availablePlants:.2f} added to garden.")
 
     countdown(loop_sleep_seconds)
     
