0d0c87de12dfcdb2c48e284c68b1e0054247396a
Casper Nybroe <casper.nybroe@cb.dk>
2022-02-28 21:48:15 +0100

    Added logs

--- plant.py
+++ plant.py
@@ -6,7 +6,7 @@ import time
 
 garden_contract_addr = "0x685BFDd3C2937744c13d7De0821c83191E3027FF"
 wallet_public_addr = "0x361472B5784e83fBF779b015f75ea0722741f304"
-min_plant_amount = 10
+min_plant_amount = 10.00
 loop_sleep_seconds = 5 # 60*60 # One hour
 
 # load private key
@@ -27,7 +27,7 @@ def available_seeds():
     return garden_contract.functions.getUserSeeds(wallet_public_addr).call()
 
 def plant():
-    txn = garden_contract.functions.roll().buildTransaction(c.get_tx_options(wallet_public_addr, 500000))
+    txn = garden_contract.functions.plantSeeds(wallet_public_addr).buildTransaction(c.get_tx_options(wallet_public_addr, 500000))
     return c.send_txn(txn, wallet_private_key)
 
 def countdown(t):
@@ -45,19 +45,23 @@ while True:
     available = available_seeds()
     available_plants = available / seedsFor1Plant
 
+    print(f"{seedsFor1Plant:.2f} seeds for 1 plant!")
+    print(f"{available:.2f} available seeds!")
+    print(f"{available_plants:.2f} available plants!")
+
     dateTimeObj = datetime.now()
     timestampStr = dateTimeObj.strftime("[%d-%b-%Y (%H:%M:%S)]")
     
     if available_plants > min_plant_amount and available >= seedsFor1Plant:
         # plant()
        
-        print(f"{timestampStr} Planted! {available_plants:.3f} added to garden.")
+        print(f"{timestampStr} Planted! {available_plants:.2f} added to garden.")
         #print(f"{timestampStr} Total value of your deposit is now ${total_value:,.2f}")
     else:
         if available_plants < min_plant_amount:
-            print(f"{timestampStr} Only {available:.3f} seeds is available for the minimum required amount: {(min_plant_amount * seedsFor1Plant):.3f}. Sleeps..")
+            print(f"{timestampStr} Only {available:.2f} seeds is available for the minimum required amount: {(min_plant_amount * seedsFor1Plant):.2f}. Sleeps..")
         else:
-            print(f"{timestampStr} Planting not ready {available:.3f} seeds available. Need {((min_plant_amount * seedsFor1Plant) - available):.3f} more")
+            print(f"{timestampStr} Planting not ready {available:.2f} seeds available. Need {((min_plant_amount * seedsFor1Plant) - available):.2f} more")
 
     countdown(loop_sleep_seconds)
     
