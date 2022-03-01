f83483e0193bc33e8d5eb0c2571487998a53a2d7
unknown <casper>
2022-03-01 09:10:20 +0100

    Added longer timer

--- plant.py
+++ plant.py
@@ -6,10 +6,11 @@ import time
 
 garden_contract_addr = "0x685BFDd3C2937744c13d7De0821c83191E3027FF"
 wallet_public_addr = "0x361472B5784e83fBF779b015f75ea0722741f304"
-min_plant_amount = 3.00
+min_plant_amount = 1.00
 loop_sleep_seconds = 60
-margin_of_error = 0.01
+margin_of_error = 0.001
 seeds_per_day_per_plant = 86400
+start_polling_threshold_in_seconds = 60*10
 
 # load private key
 wallet_private_key = open('key.txt', "r").readline()
@@ -35,11 +36,15 @@ def plant():
     txn = garden_contract.functions.plantSeeds(wallet_public_addr).buildTransaction(c.get_tx_options(wallet_public_addr, 500000))
     return c.send_txn(txn, wallet_private_key)
 
+def buildTimer(t):
+    mins, secs = divmod(int(t), 60)
+    hours, mins = divmod(int(mins), 60)
+    timer = '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs)
+    return timer
+
 def countdown(t):
     while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
+        print(buildTimer(t), end="\r")
         time.sleep(1)
         t -= 1
 
@@ -59,15 +64,12 @@ while True:
     hoursUntilPlanting = daysUntilPlanting * 24 
     secondsUntilPlanting = hoursUntilPlanting * 60 * 60
 
-    mins, secs = divmod(int(secondsUntilPlanting), 60)
-    hours, mins = divmod(int(mins), 60)
-    timer = '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs)
-
     dateTimeObj = datetime.now()
     timestampStr = dateTimeObj.strftime("[%d-%b-%Y (%H:%M:%S)]")
+
+    sleep = loop_sleep_seconds
     
     print("********** STATS *******")
-    print(f"{timestampStr} Seeds for 1 plant: {seedsFor1Plant:.3f}")
     print(f"{timestampStr} Seeds per day: {seedsPerDay:.3f}")
     print(f"{timestampStr} Planted plants: {plantedPlants:.3f}")
     print(f"{timestampStr} Available seeds: {available:.3f}")
@@ -76,16 +78,17 @@ while True:
     print(f"{timestampStr} Plants needed before planting: {plantsNeededForPlanting:.3f}")
     print(f"{timestampStr} Seeds needed before planting: {seedsNeededForPlanting:.3f}")
     print(f"{timestampStr} Seconds until next planting: {secondsUntilPlanting:.3f}")
-    print(f"{timestampStr} Until next planting:")
-    print(timer)
+    print(f"{timestampStr} Until next planting: {buildTimer(secondsUntilPlanting)}")
     print("************************")
-    
-          #  3.00      >=       3.00                  3.00       <         3.01
+
+    if secondsUntilPlanting > start_polling_threshold_in_seconds:
+        sleep = secondsUntilPlanting - start_polling_threshold_in_seconds
+            
     if availablePlants >= min_plant_amount and availablePlants < (min_plant_amount + margin_of_error):
         plant()
         print("********** PLANTED *******")
         print(f"Added {availablePlants:.2f} plants to the garden!")
         print("**************************")
 
-    countdown(loop_sleep_seconds)
+    countdown(int(sleep))
     
